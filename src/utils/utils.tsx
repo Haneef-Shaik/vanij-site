9004e01fd10d02ad3630248ad9c870c1b6f9ff9d
Vishal <vishalvk1256@gmail.com>
2024-10-21 11:10:00 +0530

    changes

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -2,10 +2,9 @@ import { twMerge } from "tailwind-merge";
 import clsx, { ClassValue } from "clsx";
 
 export function classNames(...classes: Array<string>): string {
-    return classes.filter(Boolean).join(" ");
-  }
-  
-  export function cn(...inputs: ClassValue[]): string {
-    return twMerge(clsx(inputs));
-  }
-  
+  return classes.filter(Boolean).join(" ");
+}
+
+export function cn(...inputs: ClassValue[]): string {
+  return twMerge(clsx(inputs));
+}
